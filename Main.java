63b25ef89915e8e0bfddd1651679ac04fe253173
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2021-07-15 19:44:21 -0300

    teste conflito

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Teste");
-		System.out.println("hotfix");	
+		System.out.println("hotfix");
+		System.out.println("main");
 	}
 	
 }
